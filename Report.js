216b741f442178432455484055ec4bbb7c929e9e
Robi <robi.maliqi@hotmail.co.uk>
2022-09-02 22:44:32 +0100

    code is not finished, will come back to it, tests are failing

--- Report.js
+++ Report.js
@@ -4,16 +4,18 @@ class Report {
   }
 
   generate() {
-    if (this.input == "Green") {
-      return "Test 1:\nGreen: 1";
-    } else if (this.input == "Red") {
-      return "Test 1:\nRed: 1";
-    } else if (this.input == "Amber") {
-      return "Test 1:\nAmber: 1";
-    } else if (this.input == "Amber, Amber") {
-      return "Test 1:\nAmber: 2";
-    } else return "Test 1:\nAmber: 1\nRed: 1";
+    let a = [];
+
+    let str = this.input.replace(/,/g, "");
+    str.split(" ").forEach((word) => {
+      a[word] = (a[word] || 0) + 1;
+    });
+    return a;
   }
 }
+
+const report = new Report();
+report.addScores("Amber, Red, Red, Red");
+console.log(report.generate());
 
 module.exports = Report;
