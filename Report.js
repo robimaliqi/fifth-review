1e5915f8db2bef8a715ad2a9853a0e23c16c0c5a
Robi <robi.maliqi@hotmail.co.uk>
2022-09-02 19:41:32 +0100

    made a third test and hard coded so that the test passes

--- Report.js
+++ Report.js
@@ -6,7 +6,9 @@ class Report {
   generate() {
     if (this.input == "Green") {
       return "Test 1:\nGreen: 1";
-    } else return "Test 1:\nRed: 1";
+    } else if (this.input == "Red") {
+      return "Test 1:\nRed: 1";
+    } else return "Test 1:\nAmber: 1";
   }
 }
 
--- Report.test.js
+++ Report.test.js
@@ -12,5 +12,10 @@ describe("report", () => {
       report.addScores("Red");
       expect(report.generate()).toEqual("Test 1:\nRed: 1");
     });
+    it("returns the number of times a score is input", () => {
+      const report = new Report();
+      report.addScores("Amber");
+      expect(report.generate()).toEqual("Test 1:\nAmber: 1");
+    });
   });
 });
